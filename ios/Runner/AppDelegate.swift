518dd41fdb5df4a8243d3697f13ed1a99c148d85
김서연 <kimseoyeon@gimseoyeon-ui-MacBookPro.local>
2024-11-26 19:15:24 +0900

    서연 firebase 초기 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import UIKit
-import Flutter
-import flutter_local_notifications
-
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in }
-        }
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-}
-=======
 import Flutter
 import UIKit
 
@@ -30,4 +11,3 @@ import UIKit
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
->>>>>>> 5b870a1ffa8069647f78f56873df04c533949d49
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart'; // Provider 패키지 추가
 import 'soom.dart';
@@ -6,6 +7,7 @@ import 'diffuser_state.dart'; // DiffuserState 클래스 import
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(
     ChangeNotifierProvider(
